0d25199983842c8756378643578ac67fad3ae499
frasnym <frastyawan.nym@gmail.com>
2020-12-04 09:43:04 +0800

    Header Setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Layout from "./components/Layout";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Layout>
+				<h1>Halo Dunia</h1>
+			</Layout>
+		</div>
+	);
 }
 
 export default App;
