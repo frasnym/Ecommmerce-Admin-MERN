d29bcf77058a576f132101a08b40ffe74a971def
frasnym <frastyawan.nym@gmail.com>
2020-12-04 10:16:35 +0800

    Navbar Setup

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,25 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { NavLink, Link } from "react-router-dom";
 
 export default function Header() {
 	return (
 		<Navbar bg="light" expand="lg">
 			<Container>
-				<Navbar.Brand href="#home">Admin Dashboard</Navbar.Brand>
-				{/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">
-                                Action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">
-                                Something
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse> */}
+				<Link to="/" className="navbar-brand">
+					Admin Dashboard
+				</Link>
 				<Nav>
-					<Nav.Link href="#deets">Sign In</Nav.Link>
+					<li className="nav-item">
+						<NavLink to="/signin" className="nav-link">
+							Sign In
+						</NavLink>
+					</li>
+					<li className="nav-item">
+						<NavLink to="/signup" className="nav-link">
+							Sign Up
+						</NavLink>
+					</li>
 				</Nav>
 			</Container>
 		</Navbar>
